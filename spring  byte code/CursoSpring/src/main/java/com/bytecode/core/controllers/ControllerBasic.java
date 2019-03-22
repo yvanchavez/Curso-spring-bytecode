975ae59ceedd45524adb40e4f19642ce0ce3aee7
yvan <chavez.yvan@gmail.com>
2019-03-22 12:06:45 -0500

    hola mundo

---
+++ spring  byte code/CursoSpring/src/main/java/com/bytecode/core/controllers/ControllerBasic.java
@@ -0,0 +1,15 @@
+package com.bytecode.core.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hola")
+public class ControllerBasic {
+	
+	@GetMapping(path="/holamundo")
+	public String saludar() {
+		return "index";
+	}
+}
